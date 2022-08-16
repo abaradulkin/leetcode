23eb032df4ecb532fe527ec38f2b92e4c203fc10
aliaksei-baradulkin <aliaksei.baradulkin@gmail.com>
2022-08-17 01:38:14 +0300

    Task#6 Add alternative solution

--- test_6_zigzag_conversation.py
+++ test_6_zigzag_conversation.py
@@ -14,14 +14,29 @@ def convert(input, rows_num):
     result = []
     step = (rows_num - 1) * 2
     for line_num in range(rows_num):
-        for i in range(0, len(input) - 1, step):
-            left_candidate = i + line_num
-            if left_candidate < len(input) and left_candidate not in result:
-                result.append(left_candidate)
+        for i in range(0, len(input) - line_num, step):
+            result.append(i + line_num)
             right_candidate = i + step - line_num
-            if right_candidate < len(input) and right_candidate not in result:
+            if line_num != 0 and line_num != (rows_num - 1) and right_candidate < len(input):
                 result.append(right_candidate)
     return "".join([input[i] for i in result])
+
+
+def convert_alternative(input, rows_num):
+    if rows_num == 1 or len(input) <= 1:
+        return input
+
+    result = [""] * rows_num
+    current_row = 0
+    going_down = False
+
+    for char in input:
+        result[current_row] += char
+        if current_row == 0 or current_row == rows_num - 1:
+            going_down = not going_down
+        current_row += 1 if going_down else -1
+
+    return "".join(result)
 
 
 @pytest.mark.parametrize("input, rows_num, expected", [("A", 1, "A"),
@@ -35,3 +50,17 @@ def convert(input, rows_num):
                                                        ("AA", 2, "AA")])
 def test_main_func(input, rows_num, expected):
     assert convert(input, rows_num) == expected
+
+
+@pytest.mark.parametrize("input, rows_num, expected", [("A", 1, "A"),
+                                                       ("AA", 1, "AA"),
+                                                       ("", 1, ""),
+                                                       ("", 10, ""),
+                                                       ("ABCDEFGHI", 1, "ABCDEFGHI"),
+                                                       ("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR"),
+                                                       ("PAYPALISHIRING", 4, "PINALSIGYAHRPI"),
+                                                       ("PAYPALISHIRING", 5, "PHASIYIRPLIGAN"),
+                                                       ("AA", 2, "AA")])
+def test_alt_func(input, rows_num, expected):
+    assert convert_alternative(input, rows_num) == expected
+
